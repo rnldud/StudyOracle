ed13955b248a96937fccf1524524e6c2c52ad10f
rnldud <rnldud97@naver.com>
2022-01-12 17:55:05 +0900

    2일차 완료

--- day2/SQL_실습.sql
+++ day2/SQL_실습.sql
@@ -31,6 +31,71 @@ SELECT ename, job, sal, sal*12 AS annual
 FROM emp
 ORDER BY sal DESC;    --ASC 오름차순 / DESC 내림차순
 
+SELECT ename, job, sal, sal*12 AS annual 
+FROM emp
+WHERE sal != 1000;    --<>,!=는 많이 쓰임 ^=거의 안쓰임
 
 
+SELECT ename, job, sal, sal*12 AS annual 
+FROM emp
+WHERE NOT sal = 1000;
+
+--IN
+SELECT ename, job, sal, sal*12 AS annual 
+FROM emp
+WHERE sal IN (800,1600,5000);
+
+SELECT ename, job, sal, sal*12 AS annual 
+FROM emp
+WHERE sal = 800 OR sal = 1600 OR sal =5000;
+
+
+--BETWEEN A AND B
+SELECT ename, job, sal, sal*12 AS annual 
+FROM emp
+WHERE sal>=1600 AND sal<= 2975;
+
+SELECT ename, job, sal, sal*12 AS annual 
+FROM emp
+WHERE sal BETWEEN 1600 AND 2975;
+
+-- LIKE
+SELECT ename, job, sal, sal*12 AS annual 
+FROM emp
+WHERE ENAME = 'JONES';
+
+SELECT ename, job, sal, sal*12 AS annual 
+FROM emp
+WHERE ENAME LIKE 'J%';
+
+SELECT ename, job, sal, sal*12 AS annual 
+FROM emp
+WHERE ENAME LIKE '%ER';
+
+SELECT ename, job, sal, sal*12 AS annual 
+FROM emp
+WHERE ename LIKE '__RD';             --J%, %ER, %E%, __RD
+
+--NULL, 함수명 NVL() 나중에 to be continued
+SELECT ename,job,sal,comm
+From emp
+WHERE comm IS NULL ;
+
+SELECT ename,job,sal,comm
+From emp
+WHERE comm IS NOT NULL ;
+
+--집합
+SELECT empno, ename, job FROM emp
+UNION 
+SELECT deptno,dname,loc FROM dept;
+
+SELECT empno, ename, job FROM emp
+UNION ALL
+SELECT deptno,dname,loc FROM dept;
+
+SELECT empno, ename, job FROM emp
+WHERE comm is NOT NULL
+UNION ALL
+SELECT deptno,dname,loc FROM dept;
 
